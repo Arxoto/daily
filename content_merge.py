e5959b7bf0e90d113266618a0a0e2e5f18a61b52
Arxoto <1484663586@qq.com>
2025-06-23 22:13:32 +0800

    add tips for update submodule

--- content_merge.py
+++ content_merge.py
@@ -6,6 +6,7 @@ print(subproject_path)
 sys.path.append(subproject_path)
 
 # 如果 clone 没有将子模块 拉下来 可以使用 git submodule update --init --recursive
+# 后续如果要更新子模块（拉取） git submodule update --remote --recursive
 import scripts.content_merge.main as content_merge
 
 
